41c603d54409e7ad96239e0abae01139f8673f27
Courtney Schild <courtneyschild@gmail.com>
2019-11-01 13:03:08 -0700

    finalize app functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -6,42 +6,17 @@ function functionBeepBoop(input) {
   for (index = 0; index <= input; index ++) {
     if (index.toString().includes("3")) {
       array.push(" " + "I'm sorry, Dave. I'm afraid I can't do that.");
-      console.log(array);
+    } else if (index.toString().includes("2")) {
+      array.push(" " + "Boop!");
+    } else if (index.toString().includes("1")) {
+      array.push(" " + "Beep!");
+    } else if (input === "") {
+      alert("Give me something to work with! Please enter a number.");
     } else {
       array.push(" " + index);
-      console.log(array);
     }
-    // else if (index.toString() === "2") {
-    //   array.push("Boop!");
-    //   console.log(array);
-    // } else if (index.toString() === "1") {
-    //   array.push("Beep!");
-    //   console.log(array);
-    // } else if (input === "0") {
-    //   alert("Zero. Really? Please enter a number greater than 0.");
-    // } else {
-    //   array.push();
-    // }
-
   }
   return array;
-
-
-  // let newArray = originalArray.map(function(input, i) {
-  //   if (input.includes("3")) {
-  //     originalArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
-  //     console.log(originalArray);
-  //   } else if (input.includes("2")) {
-  //     return originalArray.push("Boop!");
-  //     console.log(originalArray);
-  //   } else if (input.includes("1")) {
-  //     return originalArray.push("Beep!");
-  //     console.log(originalArray);
-  //   } else {
-  //     alert("Give me something to work with! Please enter a number.");
-  //   }
-  //   return newArray;
-  // })
 
 } // This is the end of the function.
 
@@ -56,5 +31,5 @@ $(document).ready(function() {
 
     $("#output").text(output);
 
-  }); // This is the end of the funciton.
+  }); // This is the end of the function.
 }); // This is the end of the function.
