c3064cc15aaa7004d11064e5d0851d7a4a8f5fda
Courtney Schild <courtneyschild@gmail.com>
2019-11-01 10:42:51 -0700

    add back-end functionality with branching and front-end logic with jQuery to test inputs 1, 2 and 3.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,31 @@
 // BACK-END LOGIC
 
-
+function functionBeepBoop(input) {
+  let array = [];
+  if (input.toString() === "3") {
+    array.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    console.log(array);
+  } else if (input.toString() === "2") {
+    array.push("Boop!");
+    console.log(array);
+  } else if (input.toString() === "1") {
+    array.push("Beep!");
+    console.log(array);
+  } else {
+    console.log("Shit.");
+  }
+  return array
+}
 
 // FRONT-END LOGIC
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var input = $("#number").val();
+
+    var output = functionBeepBoop(input);
+
+    $("#output").text(output);
+  });
+});
